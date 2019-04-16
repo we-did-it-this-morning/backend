6b13f5993b18d6df80d0625901b2f524c4b8d549
quantu-adunati <mcfaddenr.ebb@gmail.com>
2019-04-16 09:19:40 +0200

    Added login fucntionality

--- INF-Project/src/app/app.module.ts
+++ INF-Project/src/app/app.module.ts
@@ -5,15 +5,23 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { AdminComponent } from './admin/admin.component';
+import { NavComponent } from './nav/nav.component';
+import { LoginComponent } from './login/login.component';
+
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    AdminComponent,
+    NavComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule, HttpClientModule, 
+    AppRoutingModule, HttpClientModule,  ReactiveFormsModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ INF-Project/src/app/login/login.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { FormBuilder,Validators } from '@angular/forms';  
+import { HttpClient } from '@angular/common/http';  
+import { Router } from '@angular/router';
+
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+
+  userInfo = new FormGroup({username: new FormControl("",Validators.minLength(5)),password: new FormControl("",Validators.minLength(5))});
+  constructor(private formBuilder: FormBuilder, private http: HttpClient,private _router: Router) { }  
+  ngOnInit() {
+    this.userInfo = this.formBuilder.group({
+      userName: ['', [Validators.required]],
+      password: ['', [Validators.required]],
+    });
+  }
+
+  onSubmit()
+  {
+    if(this.userInfo.valid){
+      // this.http.post('/api/test/login-in', this.userInfo.value).subscribe((response)=>{
+      //   console.log('repsonse ',response);
+      //   if(response == true)
+      //   {
+      //     this._router.navigate(['admin']);
+      //   }
+      // });
+    }
+  }
+
+}
