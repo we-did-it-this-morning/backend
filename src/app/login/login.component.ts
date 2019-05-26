fa04aa1e1651b7e9b73abfc2714de38a9e093091
Jarrod G <u17112631@tuks.co.za>
2019-05-27 00:38:32 +0200

    connected to heroku,added welcome screen and form validators

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { LoginService } from '../login.service';
-import { map } from 'rxjs/operators';
+import { FormControl, FormGroup,FormBuilder,Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -9,13 +9,40 @@ import { map } from 'rxjs/operators';
 })
 export class LoginComponent implements OnInit {
 
-  constructor(private loginService : LoginService) { }
+  loggedIn = false;
+  loginForm : FormGroup;
+  submitted = false;
+  unauthorised = false;
+
+  constructor(private loginService : LoginService,private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.loginForm = this.formBuilder.group({
+      username: ['', [Validators.required]],
+      password: ['', [Validators.required]],
+     });
   }
 
   login(){
-    this.loginService.login().subscribe( token => this.loginService.storeToken(token["data"]["token"]));
+
+    this.submitted = true;
+
+    if(this.loginForm.invalid){
+      return;
+    }
+
+    this.loginService.login(this.loginForm.controls.username.value,this.loginForm.controls.password.value).subscribe( token => {
+      console.log(token);
+      
+      if(token == null){
+        this.unauthorised = true;
+        return;
+      }
+      this.loginService.storeToken(token["data"]["token"]);
+      this.loggedIn = true;
+    });
+    
+    
   }
 
 }
